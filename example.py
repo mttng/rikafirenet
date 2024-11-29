ae604be4523358e9fb5045496020a5438bca275d
mttng <mathieu@tanguym.eu>
2024-11-29 21:36:11 +0100

    fixed pylint error

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
+"""Exemple of how to communicate with stove"""
 import sys
 from pathlib import Path
+import asyncio
 import os
 import yaml
 import aiohttp
-import asyncio
 from rikafirenet import Stove, OperatingMode
 
 async def get_yaml_info(file):
@@ -20,42 +21,40 @@ async def main():
         # Load secrets
         secret_file = Path(os.path.dirname(__file__) + "/secret-example.yaml")
         secret = await get_yaml_info(secret_file)
-        USERNAME = secret['username']
-        PASSWORD = secret['password']
-        STOVE_ID = secret['stove_id']
-        
+        username = secret['username']
+        password = secret['password']
+        stove_id = secret['stove_id']
         # Create Stove instance
-        stove = Stove(session, USERNAME, PASSWORD, STOVE_ID)
+        stove = Stove(session, username, password, stove_id)
 
         # Connect to stove
         if not await stove.connect():
             sys.exit(1)
 
         print("")
-        print('Synching state', await stove.sync_state())  # Awaiting async sync_state method
-        print('Target temperature', await stove.get_stove_thermostat(), '°C')  # Awaiting async method
-        print('Consumption', await stove.get_stove_consumption(), 'Kg')  # Awaiting async method
-        print('Consumption before service', await stove.get_consumption_before_service(), 'Kg')  # Awaiting async method
-        print('Runtime', await stove.get_stove_runtime(), 'hours')  # Awaiting async method
-        print('Room temperature', await stove.get_room_temperature(), '°C')  # Awaiting async method
-        print('Burning', await stove.is_stove_burning())  # Awaiting async method
-        print('Flame temperature', await stove.get_stove_flame_temperature(), '°C')  # Awaiting async method
-        print('Get Operating mode', await stove.get_stove_operating_mode())  # Awaiting async method
-        print('Is on?', await stove.is_stove_on())  # Awaiting async method
-        print('Is heating times active for comfort', await stove.is_heating_times_active_for_comfort())  # Awaiting async method
-        print('State', await stove.get_state())  # Awaiting async method
-        print('WiFi signal', await stove.get_wifi_signal())  # Awaiting async method
+        print('Synching state', await stove.sync_state())
+        print('Target temperature', await stove.get_stove_thermostat(), '°C')
+        print('Consumption', await stove.get_stove_consumption(), 'Kg')
+        print('Consumption before service', await stove.get_consumption_before_service(), 'Kg')
+        print('Runtime', await stove.get_stove_runtime(), 'hours')
+        print('Room temperature', await stove.get_room_temperature(), '°C')
+        print('Burning', await stove.is_stove_burning())
+        print('Flame temperature', await stove.get_stove_flame_temperature(), '°C')
+        print('Get Operating mode', await stove.get_stove_operating_mode())
+        print('Is on?', await stove.is_stove_on())
+        print('Is heating times active for comfort', await stove.is_heating_times_active_for_comfort())
+        print('State', await stove.get_state())
+        print('WiFi signal', await stove.get_wifi_signal())
         print('')
-        
         # Uncomment the following to control the stove
-        print("Turning on", await stove.turn_on())  # Awaiting async method
-        print("Turning off", await stove.turn_off())  # Awaiting async method
-        # print(await stove.set_stove_operating_mode(OperatingMode.MANUAL.value))  # Awaiting async method
+        print("Turning on", await stove.turn_on())
+        print("Turning off", await stove.turn_off())
+        # print(await stove.set_stove_operating_mode(OperatingMode.MANUAL.value))
         print('Operating mode', OperatingMode.MANUAL.value)
-        # print('Set temperature', 20, await stove.set_stove_thermostat(20))  # Awaiting async method
-        print('Set manual power', 30, await stove.set_manual_power(30))  # Awaiting async method
-        print('Set comfort power', 3, await stove.set_confort_power(3))  # Awaiting async method
-        await stove.send_controls()  # Awaiting async method
+        # print('Set temperature', 20, await stove.set_stove_thermostat(20))
+        print('Set manual power', 30, await stove.set_manual_power(30))
+        print('Set comfort power', 3, await stove.set_confort_power(3))
+        await stove.send_controls()
 
 # Run the async main function
 if __name__ == "__main__":
